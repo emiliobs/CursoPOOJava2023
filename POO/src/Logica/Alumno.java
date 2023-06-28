e3ba0ec686717c5120dd3d3056d68efb287c437e
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-06-28 09:55:56 -0500

    Co saber que son Metodos con parametro y sin ellos

--- POO/src/Logica/Alumno.java
+++ POO/src/Logica/Alumno.java
@@ -6,5 +6,20 @@ public class Alumno
     String nombre;
     String apellido;
     
+    public  void MostrarNombre()
+    {
+        System.out.println("Hola soy un Alumno y se decir mi nombre");
+    }
     
+    public  void SaberAprobado(double  calificacion)
+    {
+        if (calificacion >= 5)
+        {
+            System.out.println("Aprobé la Materia");
+        }
+        else
+        {
+            System.out.println("Uyy no Aprobé");    
+        }
+    }
 }

5befda1cf777173d19d575e5b4f4b84322afe3f5
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-06-28 11:22:22 -0500

    todo sobre constructores y ocrear objectos

--- POO/src/Logica/Alumno.java
+++ POO/src/Logica/Alumno.java
@@ -5,6 +5,17 @@ public class Alumno
     int id;
     String nombre;
     String apellido;
+
+    public Alumno()
+    {
+    }
+
+    public Alumno(int id, String nombre, String apellido)
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
     
     public  void MostrarNombre()
     {
--- POO/src/poo/POO.java
+++ POO/src/poo/POO.java
@@ -1,11 +1,16 @@
 package poo;
 
+import Logica.Alumno;
+
 public class POO
 {
 
     public static void main(String[] args)
     {
+        Alumno alumno = new Alumno();
+        Alumno alumno1 = new Alumno(55555, "Emilio", "Barrera");
         
+        System.out.println("Los datos del alumnos so, NOmbre: : " );
     }
     
 }

024982d5d53ea6342ef308f89ac4517db7001cda
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-08-01 18:14:19 -0500

    Update Conversiones.java

--- Conversiones/src/conversiones/Conversiones.java
+++ Conversiones/src/conversiones/Conversiones.java
@@ -8,6 +8,7 @@ public class Conversiones
 
     public static void main(String[] args)
     {
+        System.out.println("Casteo de Double a Int y Long");
        double num = 1.67;
        
        //casteo a entero:
@@ -19,6 +20,33 @@ public class Conversiones
         System.out.println("Double : " + num);
         System.out.println("int    : " + numInt);
         System.out.println("Long   : " + numLong);
+        
+        System.out.println("");
+        System.out.println("=========================================================");
+        System.out.println("");
+        
+        System.out.println("Casteo de String a Double , Int y Long");
+        String cantidad = "15";
+        String precio = "150.27";
+        
+        int canEntero = Integer.parseInt(cantidad);
+        double precioDouble = Double.parseDouble(precio);
+        
+        System.out.println("El valor total de la compra es: " + (canEntero * precioDouble));
+        
+        System.out.println("");
+        System.out.println("=========================================================");
+        System.out.println("");
+        
+        System.out.println("Casteo  Double , Int y Long a String");
+        int edad = 55;
+        double estatura = 1.67;
+        
+        String edaString = String.valueOf(edad);
+        String estaturaString = String.valueOf(estatura);
+        
+        System.out.println("Ead :" + edaString  + " Estatura : " + estaturaString);
+        
     }
     
 }

010cbb05967f79027c94885585f50b1a052708b8
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-08-01 18:02:16 -0500

    CAsteos de long. int, double

---
+++ ConsumoAPI/src/consumoapi/ConsumoAPI.java
@@ -0,0 +1,23 @@
+
+package consumoapi;
+
+
+public class ConsumoAPI
+{
+
+  
+    public static void main(String[] args)
+    {
+       // Solicitar uan peticion:
+        
+        
+        
+        
+       // Peticoin Correcta
+        
+       // Abir un scaner que le el flujo de datos
+       
+       // Mostrar la informacion obtenida por consola 
+    }
+    
+}
---
+++ Conversiones/src/conversiones/Conversiones.java
@@ -0,0 +1,24 @@
+
+package conversiones;
+
+
+public class Conversiones
+{
+
+
+    public static void main(String[] args)
+    {
+       double num = 1.67;
+       
+       //casteo a entero:
+       int numInt = (int)num;
+       
+       //CAsteo a long:
+       long numLong = (long)num;
+       
+        System.out.println("Double : " + num);
+        System.out.println("int    : " + numInt);
+        System.out.println("Long   : " + numLong);
+    }
+    
+}
